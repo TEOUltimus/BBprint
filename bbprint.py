778964a4224e5924ac3bec5379bbfdf80e51fbd6
Eric D Summers <esummers@unix3.andrew.cmu.edu>
2014-01-31 19:42:02 -0500

    checks for usrname & pwd

--- bbprint.py
+++ bbprint.py
@@ -1,8 +1,13 @@
-from pyquery import PyQuery as pq
+'''from pyquery import PyQuery as pq
 from auth import authenticate
 from datetime import datetime
-from urllib import urlencode
-import re
-import json
+from urllib import urlencode'''
+import re, json, os, sys
+
+try:
+	usr = sys.argv.index('-u') + 1
+	pwd = sys.argv.index('-p') + 1
+except ValueError:
+	print 'Username and password not detected.'
 
 
